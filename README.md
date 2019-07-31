73f16d64e242a767a73516045cff7edef2d4451c
wk-j <somnuk.wk@gmail.com>
2019-07-31 18:49:43 +0700

    Refactor

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Override property
 
 ```bash
 mvn package
+java -jar target/app-1.0.1.jar
 java -Dconnection=xyz -jar target/app-1.0.1.jar
 ```
--- src/main/java/wk/Program.java
+++ src/main/java/wk/Program.java
@@ -5,18 +5,23 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Program {
-    public static void main(String[] args) {
-        try (InputStream resourceAsStream = Program.class.getClassLoader().getResourceAsStream("db.properties")) {
+
+    static Properties load(String file) {
+        try (InputStream resourceAsStream = Program.class.getClassLoader().getResourceAsStream(file)) {
             Properties prop = new Properties();
             prop.load(resourceAsStream);
 
             Properties sys = System.getProperties();
             prop.putAll(sys);
-
-            prop.forEach((k, v) -> System.out.println(k + " = " + v));
-
+            return prop;
         } catch (IOException e) {
             System.err.print(e.getMessage());
+            return new Properties();
         }
+    }
+
+    public static void main(String[] args) {
+        Properties props = load("db.properties");
+        System.out.println(props.getProperty("connection"));
     }
 }
