6f5e3c830f0fcc0ddef520a1d296331dc0c89896
wk-j <somnuk.wk@gmail.com>
2019-07-31 18:30:43 +0700

    Resource

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ```bash
 mvn package
+java -jar target/app-1.0.1.jar -Dconnection=127.0.0.1
 ```
--- src/main/java/wk/Program.java
+++ src/main/java/wk/Program.java
@@ -1,7 +1,18 @@
 package wk;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try (InputStream resourceAsStream = Program.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties prop = new Properties();
+            prop.load(resourceAsStream);
+
+            System.err.println(prop.getProperty("connection"));
+        } catch (IOException e) {
+            System.err.print(e.getMessage());
+        }
     }
 }
